2e67303a8688d3e5978c11bf723aff219e92f133
Violette Dunoyer de S <violette.dunoyer@gmail.com>
2022-05-23 16:55:46 +0200

    finished create and new

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -4,7 +4,25 @@ class DucksController < ApplicationController
   end
 
   def show
-    @ducks = Duck.find(params[:id])
+    @duck = Duck.find(params[:id])
   end
 
+  def new
+    @duck = Duck.new
+  end
+
+  def create
+    @duck = Duck.new(duck_params)
+    if @duck.save
+      redirect_to duck_path(@duck)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def duck_params
+    params.require(:duck).permit(:name, :price, :location, :description)
+  end
 end
