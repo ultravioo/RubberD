97040da80753b368bc133a671a2881185b813e7e
Fernando Gomez <fernandogomezv@gmail.com>
2022-05-24 10:31:47 +0200

    pundit gem intro

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,18 +1,22 @@
 class DucksController < ApplicationController
   def index
-    @ducks = Duck.all
+    @ducks = policy_scope(Duck)
   end
 
   def show
     @duck = Duck.find(params[:id])
+    authorize @duck
   end
 
   def new
     @duck = Duck.new
+    authorize @duck
   end
 
   def create
     @duck = Duck.new(duck_params)
+    @duck.user = current_user
+    authorize @duck
     if @duck.save
       redirect_to duck_path(@duck)
     else
---
+++ app/policies/duck_policy.rb
@@ -0,0 +1,16 @@
+class DuckPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+    def create?
+      true
+    end
+
+    def show?
+      true
+    end
+end
