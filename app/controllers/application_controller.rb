8e6eab22bf8f09c570606cda4469eecc832a3bf9
Fernando Gomez <fernandogomezv@gmail.com>
2022-05-25 14:20:15 +0200

    homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,12 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
+
+  def after_sign_in_path_for(resource)
+    ducks_path
+  end
+
+  def after_sign_up_path_for(resource)
+    ducks_path
+  end
 end
