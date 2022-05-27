b62b9608c1739b37f8245d1b1f23af98b07166e9
Fernando Gomez <fernandogomezv@gmail.com>
2022-05-27 15:02:54 +0200

    pushed to master shouldn't happen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+  end
+
   private
 
   def skip_pundit?
