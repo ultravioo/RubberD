dcf38f55726fc099bba3df8510a03e299691b024
Violette Dunoyer de S <violette.dunoyer@gmail.com>
2022-05-27 11:23:02 +0200

    add review model

---
+++ db/migrate/20220527091949_create_reviews.rb
@@ -0,0 +1,10 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reviews do |t|
+      t.string :content
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
